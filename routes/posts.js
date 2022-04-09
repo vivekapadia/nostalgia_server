8d7e125d7aa7c6cbda8b6e79ff495a3eafd7e834
vivekapadia <610vivek@gmail.com>
2022-04-09 17:03:20 +0530

    controllers remove logic from routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,10 @@
 import express from "express";
 
+import { getPosts } from "../controllers/posts.js";
+
 const router = express.Router();
 
 // http://localhost:5555/posts
-router.get("/", (req, res) => {
-	res.send("This works !");
-});
+router.get("/", getPosts);
 
 export default router;
