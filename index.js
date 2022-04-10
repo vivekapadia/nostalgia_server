b2ef78aeab5675604372a44fcdc0243e1cbd7277
vivekapadia <610vivek@gmail.com>
2022-04-10 18:48:18 +0530

    dotenv to protect sensitive information

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,12 +14,15 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-	"mongodb+srv://nostalgia:nostalgia@nostalgiadb.o7awm.mongodb.net/nostalgiadb?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5555;
+// heroku || local
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 mongoose
-	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(CONNECTION_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
 	.then(() =>
 		app.listen(PORT, () =>
 			console.log(`Server Running on Port: http://localhost:${PORT}`)
