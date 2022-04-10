e45079ba5150210553d7dc67a372858cae679385
vivekapadia <610vivek@gmail.com>
2022-04-10 15:41:29 +0530

    updatePost implemented

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -22,4 +23,19 @@ export const createPost = async (req, res) => {
 	} catch (error) {
 		res.status(409).json({ message: error.message });
 	}
+};
+
+// /posts/6252771747d795a47af433eb
+export const updatePost = async (req, res) => {
+	const { id } = req.params;
+	const post = req.body;
+
+	if (!mongoose.Types.ObjectId.isValid(id))
+		return res.status(404).send(`No post with id: ${id}`);
+
+	const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+		new: true,
+	});
+
+	res.json(updatedPost);
 };
