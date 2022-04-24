b5b9253fa34321a87401e1cee959f3cc1f455a0e
vivekapadia <610vivek@gmail.com>
2022-04-24 13:56:44 +0530

    signUp Logic added

--- controllers/user.js
+++ controllers/user.js
@@ -30,5 +30,30 @@ export const signin = async (req, res) => {
 };
 
 export const signup = async (req, res) => {
-	// logic
+	const { email, password, firstName, lastName } = req.body;
+
+	try {
+		const oldUser = await UserModal.findOne({ email });
+
+		if (oldUser)
+			return res.status(400).json({ message: "User already exists" });
+
+		const hashedPassword = await bcrypt.hash(password, 12);
+
+		const result = await UserModal.create({
+			email,
+			password: hashedPassword,
+			name: `${firstName} ${lastName}`,
+		});
+
+		const token = jwt.sign({ email: result.email, id: result._id }, secret, {
+			expiresIn: "1h",
+		});
+
+		res.status(201).json({ result, token });
+	} catch (error) {
+		res.status(500).json({ message: "Something went wrong" });
+
+		console.log(error);
+	}
 };
