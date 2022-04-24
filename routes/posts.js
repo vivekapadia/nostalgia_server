95afcdf1a4c01bc9ed18330dbffa7720e7219032
vivekapadia <610vivek@gmail.com>
2022-04-24 14:11:08 +0530

    auth middleware passed to routes

--- routes/posts.js
+++ routes/posts.js
@@ -10,10 +10,12 @@ import {
 
 const router = express.Router();
 
+import auth from "../middleware/auth.js";
+
 router.get("/", getPosts);
-router.post("/", createPost);
-router.patch("/:id", updatePost);
-router.delete("/:id", deletePost);
-router.patch("/:id/likePost", likePost);
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
 
 export default router;
