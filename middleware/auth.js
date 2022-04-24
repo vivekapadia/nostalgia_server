d253e7d61a9c9bc4072f3b94f9bcdc8f88817c65
vivekapadia <610vivek@gmail.com>
2022-04-24 16:28:16 +0530

    minor change

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,29 @@
 import jwt from "jsonwebtoken";
 
-const secret = 'test';
+const secret = "test";
 
 const auth = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    const isCustomAuth = token.length < 500;
+	try {
+		// server side its a for authorization
+		const token = req.headers.authorization.split(" ")[1];
+		const isCustomAuth = token.length < 500;
 
-    let decodedData;
+		let decodedData;
 
-    if (token && isCustomAuth) {      
-      decodedData = jwt.verify(token, secret);
+		if (token && isCustomAuth) {
+			decodedData = jwt.verify(token, secret);
 
-      req.userId = decodedData?.id;
-    } else {
-      decodedData = jwt.decode(token);
+			req.userId = decodedData?.id;
+		} else {
+			decodedData = jwt.decode(token);
 
-      req.userId = decodedData?.sub;
-    }    
+			req.userId = decodedData?.sub;
+		}
 
-    next();
-  } catch (error) {
-    console.log(error);
-  }
+		next();
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export default auth;
